fef8a6afabfe3e6ab371164dd8d4b6eecb17f284
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-25 20:54:51 +0500

    Remove console logs from MongoDB connection utility

--- identity-service/src/config/db.js
+++ identity-service/src/config/db.js
@@ -6,12 +6,8 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}`
     );
     logger.info(`Connected to mongodb :${connectionInstance.connection.host}`)
-    console.log(
-      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
-    );
   } catch (error) {
     logger.error('MongoDb connection error',error)
-    console.log("MONGODB connection FAILED ", error);
     process.exit(1);
   }
 };
