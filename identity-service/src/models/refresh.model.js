672c56c68dcd051f0550240f641aef060537ceb1
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-25 12:01:24 +0500

    Add refresh token model for managing token persistence

---
+++ identity-service/src/models/refresh.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const refreshTokenSchema=new mongoose.Schema({
+  token:{
+    type:String,
+    required:true
+  },
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+  },
+  expiresAt:{
+    type:Date,
+    required:true
+  }
+},{timestamps:true})
+
+refreshTokenSchema.index({expirexAt:1},{expireAfterSeconds:0})
+
+export const RefreshToken=mongoose.model('"RefreshToken',refreshTokenSchema)
