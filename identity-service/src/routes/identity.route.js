34a581952490bfc6ea9edebff80b5f0c80515f1b
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-24 15:28:35 +0500

    Add logout route to identity service with authentication middleware

--- identity-service/src/routes/identity.route.js
+++ identity-service/src/routes/identity.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/identity.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/identity.controller.js";
+import { isAuth } from "../middleware/auth.middleware.js";
 
 const identityRouter = express.Router();
 
 identityRouter.post("/register", registerUser);
 identityRouter.post("/login", loginUser);
+identityRouter.get("/logout", isAuth,logoutUser);
+
 
 export default identityRouter;
