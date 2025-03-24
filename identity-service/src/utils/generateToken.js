58c5a5b0287124f7550df7396bb8a3e333446cde
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-24 15:28:26 +0500

    Refactor token generation to use JWT for refresh tokens and update user schema to store refreshToken

--- identity-service/src/utils/generateToken.js
+++ identity-service/src/utils/generateToken.js
@@ -1,19 +1,18 @@
 import jwt from "jsonwebtoken";
-import crypto from "crypto";
-import { RefreshToken } from "../models/refresh.model.js";
 export const generateToken = async (user) => {
   const accessToken = jwt.sign(
     { userId: user._id, username: user.username },
-    process.env.JWT_SECRET,
+    process.env.ACCESS_JWT_SECRET,
     { expiresIn: "60m" }
   );
-  const refreshToken = crypto.randomBytes(40).toString("hex");
-  const expiresAt = new Date();
-  expiresAt.setDate(expiresAt.getDate() * 7); //refresh token expires in 7 days
-  await RefreshToken.create({
-    token: refreshToken,
-    user: user._id,
-    expiresAt,
-  });
+  const refreshToken = jwt.sign(
+    { userId: user._id, username: user.username },
+    process.env.REFRESH_JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+  user.refreshToken = refreshToken;
+  await user.save({ validateBeforeSave: false });
   return { accessToken, refreshToken };
 };
