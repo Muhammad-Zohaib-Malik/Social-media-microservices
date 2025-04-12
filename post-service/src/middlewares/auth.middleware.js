353d9852fa627839396a64ba11ee77baff0a8b06
zohaib <muhammadzohaibmalik10@gmail.com>
2025-04-13 02:58:16 +0500

    added auth middleware

---
+++ post-service/src/middlewares/auth.middleware.js
@@ -0,0 +1,14 @@
+import logger from "../../../api-gateway/src/utils/logger.js";
+
+export const authenticateRequest = (req, res, next) => {
+    const userId = req.headers["x-user-id"];
+    if (!userId) {
+        logger.warn(`Access attempted without user ID`);
+        return res.status(401).json({
+          success: false,
+          message: "Authencation required! Please login to continue",
+        });
+      }
+      req.user = { userId };
+      next();
+}
