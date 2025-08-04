c4bfbad2e4e620f837181a7a8e576daa8aeeaaf7
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-04 12:25:14 +0500

    refactor: remove refreshToken generation from token utility

--- identity-service/src/utils/generateToken.js
+++ identity-service/src/utils/generateToken.js
@@ -5,14 +5,5 @@ export const generateToken = async (user) => {
     process.env.ACCESS_JWT_SECRET,
     { expiresIn: "60m" }
   );
-  const refreshToken = jwt.sign(
-    { userId: user._id, username: user.username },
-    process.env.REFRESH_JWT_SECRET,
-    {
-      expiresIn: "7d",
-    }
-  );
-  user.refreshToken = refreshToken;
-  await user.save({ validateBeforeSave: false });
-  return { accessToken, refreshToken };
+  return { accessToken };
 };
