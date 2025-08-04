65f371e3a5cbc9fcca391725f87aef5c67239dce
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-04 12:25:57 +0500

    refactor: remove unused refreshToken field from user selection in auth middleware

--- identity-service/src/middleware/auth.middleware.js
+++ identity-service/src/middleware/auth.middleware.js
@@ -11,9 +11,9 @@ export const isAuth = async (req, res, next) => {
         .status(404)
         .json({ success: false, message: "Token is missing" });
     }
- 
+
     const decodedToken = jwt.verify(token, process.env.ACCESS_JWT_SECRET);
-    const user = await User.findById(decodedToken.userId).select("-password -refreshToken");
+    const user = await User.findById(decodedToken.userId).select("-password");
 
     if (!user) {
       return res.status(404).json({ success: false, message: "Invalid user" });
