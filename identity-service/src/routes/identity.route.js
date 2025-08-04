351ff2a3a3913665ccf7f157a6c1a3f9910f051a
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-04 12:23:07 +0500

    format code for better readability in identity route

--- identity-service/src/routes/identity.route.js
+++ identity-service/src/routes/identity.route.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/identity.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/identity.controller.js";
 import { isAuth } from "../middleware/auth.middleware.js";
-
 const identityRouter = express.Router();
 
 identityRouter.post("/register", registerUser);
 identityRouter.post("/login", loginUser);
-identityRouter.get("/logout", isAuth,logoutUser);
-
+identityRouter.get("/logout", isAuth, logoutUser);
 
 export default identityRouter;
