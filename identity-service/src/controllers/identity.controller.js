a0cdb9e5e5f3899a6fd0086a881dff94a9834faa
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-04 15:47:47 +0500

    refactor: remove user object from login response

--- identity-service/src/controllers/identity.controller.js
+++ identity-service/src/controllers/identity.controller.js
@@ -80,7 +80,6 @@ export const loginUser = async (req, res) => {
       success: true,
       message: "User login successfully",
       accessToken,
-      user,
     });
   } catch (error) {
     logger.error("Login error occur", error);
