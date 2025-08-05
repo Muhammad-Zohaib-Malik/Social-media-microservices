fe85ef16e704253fdf336aa657128150ee988fb1
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-05 08:16:32 +0500

    refactor: streamline middleware setup and remove unused Redis rate limiting

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,23 +1,20 @@
-import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import Redis from "ioredis";
 import helmet from "helmet";
-import { rateLimit } from "express-rate-limit";
-import { RedisStore } from "rate-limit-redis";
 import logger from "./utils/logger.js";
 import proxy from "express-http-proxy";
+import { rateLimiter } from "./utils/rateLimiting.js";
+import { isAuth } from "./middleware/auth.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 3000;
-const redisClient = new Redis(process.env.REDIS_URL);
 
 // middleware
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   logger.info(`Received ${req.method} request to ${req.url}`);
@@ -25,22 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// ip based  rate limiting for sensitive endpoints
-const rateLimitOptions = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-  handler: (req, res) => {
-    logger.warn(`Sensitive endpoint rate limit exceeded for ip :${req.ip}`);
-    res.status(429).json({ success: false, message: "Too many requests" });
-  },
-  store: new RedisStore({
-    sendCommand: (...args) => redisClient.call(...args),
-  }),
-});
-
-app.use(rateLimitOptions);
+app.use(rateLimiter)
 
 const proxyOPtions = {
   proxyReqPathResolver: (req) => {
@@ -72,8 +54,33 @@ app.use(
   })
 );
 
+// setting up proxy for our post service
+
+app.use(
+  "/v1/posts",isAuth,
+  proxy(process.env.POST_SERVICE_URL, {
+    ...proxyOPtions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from identity service: ${proxyRes.statusCode}`
+      );
+      return proxyResData;
+    },
+  })
+);
+
 app.listen(PORT, () => {
   logger.info(`Api gateway listening on port ${PORT}`);
-  logger.info(`Proxying requests to ${process.env.IDENTITY_SERVICE_URL}`);
-  logger.info(`Redis url ${process.env.REDIS_URL}`);
+  logger.info(
+    `Identity service is running on port ${process.env.IDENTITY_SERVICE_URL}`
+  );
+  logger.info(
+    `post service is running on port ${process.env.POST_SERVICE_URL}`
+  );
 });
